69f6e5d72058d4b998d9a52678fc9ebf4df26b25
Sudip Poudel <spdd28356@gmail.com>
2026-07-01 22:35:34 +0545

    initial express server setup

---
+++ Backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to MediTrack API"
+    });
+});
+
+module.exports = app;
